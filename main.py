f79fcc9d75428ea96d33fb48444c17523fb18d4e
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-04-16 08:57:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ Builder.load_string("""
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
             height: 100
-            text: "KSquared-math,LLC © :"
+            text: "KSquared-Mathematics :"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
@@ -109,10 +109,11 @@ Builder.load_string("""
                 font_size: 75
                 size_hint_y: None
                 height: 200
-                text: "Visit KSquared,LLC"
+                padding: 10, 10
+                text: "Visit KSquared-Mathematics"
                 on_release:
                     import webbrowser
-                    webbrowser.open('https://kevinjunice.wixsite.com/ksquaredllc')
+                    webbrowser.open('https://www.ksquaredmathematics.com/subscribe') 
                     
             Button:
                 font_size: 75
@@ -130,10 +131,10 @@ Builder.load_string("""
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
-                text: "Share KSquared-math,LLC ©"
+                text: "Share KSquared-Mathematics"
                     
             Image:
-                source: 'KSquared_QR_code.png'
+                source: 'KSquared_QR.png'
                 size_hint_y: None
                 height: 1000
                 width: 1000
@@ -163,7 +164,7 @@ Builder.load_string("""
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
-                text: "What's new at KSquared-math?"
+                text: "What's new at KSquared-Mathematics?"
             
             Button:
                 id: steps
